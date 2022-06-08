32c1eb7bccad6f46d97fcb4fa0419463bbdde780
SoyeonHH <sodus1102@gmail.com>
2022-06-08 19:34:26 +0900

    Implement pickle dataset making processing

--- data/process_data.py
+++ data/process_data.py
@@ -1,3 +1,4 @@
+from itertools import product
 import pickle
 import json
 from collections import defaultdict
@@ -15,21 +16,28 @@ def rating_to_label(r):
 
 def main():
 
-    user_times = defaultdict(list)
     with open('amazon_fashion_dataset_review.json', 'r') as f:
         data = json.load(f)
+    
+    data = pd.DataFrame(data)
 
-    df = pd.DataFrame.from_dict(data)
+    data['product'] = data['product'].astype(str)
+    data['time'] = data['time'].astype(int)
+    data['year'] = data['year'].astype(int)
+    data['month'] = data['month'].astype(int)
+    data['day'] = data['day'].astype(int)
+    data['text'] = data['text'].astype(str)
+    data['rating'] = data['rating'].astype(float)
 
-    df = df[df.text.apply(lambda x: len(x.strip().split())) >= 10]
-    df['text'] = df.text.apply(lambda x: x.lower())
-    df['label'] = df['rating'].apply(rating_to_label)
-    df.drop_duplicates(subset=['text'], inplace=True)
-    df.dropna(inplace=True)
+    data = data[data['text'].apply(lambda x: len(x.strip().split())) >= 10]
+    data['text'] = data['text'].apply(lambda x: x.lower())
+    data['label'] = data['rating'].apply(rating_to_label)
+    data.drop_duplicates(subset=['text'], inplace=True)
+    data.dropna(inplace=True)
 
-    df.reset_index(inplace=True, drop=True)
+    data.reset_index(inplace=True, drop=True)
 
-    data = df[['product', 'time', 'year', 'month', 'day', 'text', 'rating', 'label']]
+    data = data[['product', 'time', 'year', 'month', 'day', 'text', 'rating', 'label']]
 
     train_dev, test = train_test_split(data, test_size=0.2, random_state=123, stratify=data[['rating']])
     train, dev = train_test_split(train_dev, test_size=0.125, random_state=123, stratify=train_dev[['rating']])
@@ -39,5 +47,22 @@ def main():
     test.to_csv('amazon_fashion_test.csv', index=False)
 
     edge_set = set()
-    users = set(data.user)
+    products = set(data['product'])
+
+    with open('amazon_fashion_dataset_product.json', 'r') as f:
+        data_product = json.load(f)
+    
+    data_product = pd.DataFrame(data_product)
+    c_product = data_product['product']
+    c_related = data_product['related'].apply(lambda x: x.strip().split(', '))
+
+    edge_set.update([(p, r) for p, rs in zip(c_product, c_related) for r in rs if p in products and r in products])
+
+    with open('amazon_fashion_edges.p', 'wb') as f:
+        pickle.dump(edge_set, f)
+    with open('amazon_fashion_products.p', 'wb') as f:
+        pickle.dump(products, f)
+
+if __name__ == '__main__':
+    main()
 
